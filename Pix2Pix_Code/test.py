c7acfacfb7874c75e882e0baa11ccd98c23320b3
Jonlenes <j211846@dac.unicamp.br>
2019-05-12 09:09:01 -0300

    Evaluation Metric

--- Pix2Pix_Code/test.py
+++ Pix2Pix_Code/test.py
@@ -6,6 +6,13 @@ from util.visualizer import save_images
 from util import html
 
 
+def fnorm(x):
+    return sqrt((x**2).sum())
+
+def score(y_true, y_pred):
+    return 100*(1 - 2*fnorm(y_true - y_pred) / (fnorm(y_true) + fnorm(y_pred))) 
+
+
 if __name__ == '__main__':
     opt = TestOptions().parse()
     # hard-code some parameters for test
@@ -26,6 +33,8 @@ if __name__ == '__main__':
     # CycleGAN: It should not affect CycleGAN as CycleGAN uses instancenorm without dropout.
     if opt.eval:
         model.eval()
+
+    total_acc, count = 0, 0
     for i, data in enumerate(dataset):
         if i >= opt.num_test:
             break
@@ -34,7 +43,14 @@ if __name__ == '__main__':
         visuals = model.get_current_visuals()
         img_path = model.get_image_paths()
         if i % 5 == 0:
-            print('processing (%04d)-th image... %s' % (i, img_path))
+            print('processing (%04d)-th image... %s' % (i, img_path), end="")
         save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
+    
+        acc = score(model.real_A, model.fake_B)
+        print("Acc:", acc)
+        total_acc += acc
+        count += 1
+
+    print("Accuracy: %.2f." % total_acc / count)
     # save the website
     webpage.save()
